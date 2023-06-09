ce757c4f1565bf868d61e67c0a7be7f3a7270865
Vitalii <vitalik.baranetskiy@gmail.com>
2023-06-09 18:28:20 +0300

    Initial commit

---
+++ src/main/groovy/com/github/vitaliibaranetskyi/socialbird/SocialBirdApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.vitaliibaranetskyi.socialbird
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SocialBirdApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SocialBirdApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/github/vitaliibaranetskyi/socialbird/SocialBirdApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.github.vitaliibaranetskyi.socialbird
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialBirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
